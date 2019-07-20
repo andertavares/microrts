0a7c8d4e23f8baaf922d37f1f17bc64a8eeff9b7
anderson <andersonrochatavares@gmail.com>
2019-07-20 12:39:04 -0300

    moving binary trace tests to their package

--- src/tests/trace/TraceGenerationTestBin.java
+++ src/tests/trace/TraceGenerationTestBin.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package tests;
+package tests.trace;
 
 import ai.core.AI;
 import ai.abstraction.WorkerRush;
